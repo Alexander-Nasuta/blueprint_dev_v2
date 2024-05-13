e8fe08db4990dd31f1b54f06d65f2fd22141976d
Alexander Nasuta <alexander.nasuta@ima.rwth-aachen.de>
2024-05-13 13:26:49 +0200

    docs first version

---
+++ docs/conf.py
@@ -0,0 +1,44 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'BluePrints'
+copyright = '2024, Alexander Nasuta, Mats Gesenhues'
+author = 'Alexander Nasuta, Mats Gesenhues'
+release = '0.2'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.todo',
+    'sphinx.ext.autosectionlabel',
+
+    'sphinx_copybutton'
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
+html_css_files = ['custom.css']
+
+html_logo = '_static/logo-optipack.svg'
+
+
+copybutton_prompt_text = ">>> "
+copybutton_prompt_is_regexp = False
---
+++ docs/index.rst
@@ -0,0 +1,25 @@
+.. BluePrints documentation master file, created by
+   sphinx-quickstart on Tue Mar 19 11:10:46 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to KiOptiPack BluePrints!
+======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   introduction
+   quickstart
+   blueprints/blueprints
+   code-docs/code-documentation
+   genindex
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
